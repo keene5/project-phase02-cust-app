70855e5d9922e5c15140891e36dc242f6a7c7be8
keene5 <31411970+keene5@users.noreply.github.com>
2024-09-20 14:43:27 -0400

    Validations

--- src/components/CustomerList.jsx
+++ src/components/CustomerList.jsx
@@ -1,36 +1,40 @@
-import React from 'react'
+import React from 'react';
 import '../App.css';
 
-function CustomerList({customers, handleListClick, selected}) {
+function CustomerList({ customers, handleListClick, selected }) {
+  const maskPassword = (password) => {
+    return '*'.repeat(password.length);
+  };
+
   return (
-      <div className="boxed">
-        <h4>Customer List</h4>
-        <table id="customer-list">
-          <thead>
-            <tr className='left-aligned'>
-              <th className='left-aligned'>Name</th>
-              <th className='left-aligned'>Email</th>
-              <th className='left-aligned'>Pass</th>
-            </tr>
-          </thead>
-          <tbody>
-            {customers.map((item, index) => {
-              return (
-                <tr
-                  key={item.id}
-                  onClick={() => handleListClick(item)}
-                  className={selected(item)}
-                >
-                  <td>{item.name}</td>
-                  <td>{item.email}</td>
-                  <td>{item.password}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-  )
+    <div className="boxed">
+      <h4>Customer List</h4>
+      <table id="customer-list">
+        <thead>
+          <tr className='left-aligned'>
+            <th className='left-aligned'>Name</th>
+            <th className='left-aligned'>Email</th>
+            <th className='left-aligned'>Pass</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customers.map((item, index) => {
+            return (
+              <tr
+                key={item.id}
+                onClick={() => handleListClick(item)}
+                className={selected(item)}
+              >
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td className='pass'>{maskPassword(item.password)}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default CustomerList
+export default CustomerList;
--- src/validate.js
+++ src/validate.js
@@ -1,58 +1,62 @@
-import validator from 'validator';
+import validator from "validator";
 import { isUnsanitaryInput } from "./sanitize.js";
 
 export const validateEmail = (email) => {
-    if (!validator.isEmail(email)) {
-      return false;
-    }
-    return true;
-  };
+  if (!validator.isEmail(email)) {
+    return false;
+  }
+  return true;
+};
 
 export const validatePassword = (password) => {
-    if (!validator.isStrongPassword(password)) {
-      return false;
-    }
-    return true;
-  };
+  if (!validator.isStrongPassword(password)) {
+    return false;
+  }
+  return true;
+};
 
-  export function determineErrors(errors, formObject) {
-    let newErrors = { ...errors };
-    let hasErrors = false;
-  
-    if (formObject.name === "") {
-      newErrors.name = "Name is required";
-      hasErrors = true;
-    } else if (isUnsanitaryInput(formObject.name)) {
-      newErrors.name = "Name contains unsanitary characters";
-      hasErrors = true;
-    }
-  
-    if (formObject.email === "") {
-      newErrors.email = "Email is required";
-      hasErrors = true;
-    } else if (isUnsanitaryInput(formObject.email)) {
-      newErrors.email = "Email contains unsanitary characters";
-      hasErrors = true;
-    } else if (!validateEmail(formObject.email)) {
-      newErrors.email = "Invalid email";
-      hasErrors = true;
-    }
-  
-    if (formObject.password === "") {
-      newErrors.password = "Password is required";
-      hasErrors = true;
-    } else if (isUnsanitaryInput(formObject.password)) {
-      newErrors.password = "Password contains unsanitary characters";
-      hasErrors = true;
-    } else if (!validatePassword(formObject.password)) {
-      newErrors.password = "Invalid password";
-      hasErrors = true;
-    }
-    return { hasErrors, newErrors };
+export function determineErrors(errors, formObject) {
+  let newErrors = { ...errors };
+  let hasErrors = false;
+
+  if (formObject.name === "") {
+    newErrors.name = "Name is required";
+    hasErrors = true;
+  } else if (!checkForLetters(formObject.name)) {
+    newErrors.name = "Name should only contains letters";
+    hasErrors = true;
+  } else if (isUnsanitaryInput(formObject.name)) {
+    newErrors.name = "Name contains unsanitary characters";
+    hasErrors = true;
   }
 
-  
+  if (formObject.email === "") {
+    newErrors.email = "Email is required";
+    hasErrors = true;
+  } else if (isUnsanitaryInput(formObject.email)) {
+    newErrors.email = "Email contains unsanitary characters";
+    hasErrors = true;
+  } else if (!validateEmail(formObject.email)) {
+    newErrors.email = "Invalid email";
+    hasErrors = true;
+  }
 
+  if (formObject.password === "") {
+    newErrors.password = "Password is required";
+    hasErrors = true;
+  } else if (isUnsanitaryInput(formObject.password)) {
+    newErrors.password = "Password contains unsanitary characters";
+    hasErrors = true;
+  } else if (!validatePassword(formObject.password)) {
+    newErrors.password = "Invalid password";
+    hasErrors = true;
+  }
+  return { hasErrors, newErrors };
+}
 
+export function checkForLetters(name) {
+  const regex = /[a-zA-Z]/;
 
+ return regex.test(name);
+  }
 
